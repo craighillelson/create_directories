c5a99f931071a5260b5b19e8512dd34cdbfd8cf2
Craig Hillelson <craighillelson@gmail.com>
2020-06-04 19:27:00 -0400

    update create_directories.py
    added code to provide an update to the user after the directories have been created

--- create_directories.py
+++ create_directories.py
@@ -5,6 +5,7 @@ from pathlib import Path
 
 def prompt_user_for_directories_to_create():
     """Prompt user for directories to be created."""
+    print('\n')
     lst = []
     while True:
         print('Enter the names of directories you\'d like to add (or nothing '
@@ -26,5 +27,8 @@ def create_directories(a) -> Path:
 
 directories = prompt_user_for_directories_to_create()
 
+print('\nThe following directories were created\n')
 for directory in directories:
     create_directories(directory)
+    print(f'\t{directory}')
+print('\n')
