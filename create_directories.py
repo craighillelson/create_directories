86c0e9332f49b473ca3552e10e3a84e4a0a23b3e
Craig Hillelson <craighillelson@gmail.com>
2020-10-21 23:09:51 -0400

    update create_directories.py
    linted code

--- create_directories.py
+++ create_directories.py
@@ -5,30 +5,31 @@ from pathlib import Path
 
 def prompt_user_for_directories_to_create():
     """Prompt user for directories to be created."""
-    print('\n')
+
     lst = []
     while True:
-        print('Enter the names of directories you\'d like to add (or nothing '
-              'to stop).')
-        directory = input('> ')
-        if directory == '':
+        print("\nEnter the names of directories you'd like to add (or nothing "
+              "to stop).")
+        usr_spec_dir = input("> ")
+        if usr_spec_dir == "":
             break
-        lst = lst + [directory]
+        lst = lst + [usr_spec_dir]
 
     return lst
 
 
-def create_directories(a) -> Path:
+def create_directories(dir_to_create) -> Path:
     """Create required directories."""
-    output_dir = Path('.') / f'{a}'
+
+    output_dir = Path(".") / f"{dir_to_create}"
     output_dir.mkdir(exist_ok=True)
     return output_dir
 
 
 directories = prompt_user_for_directories_to_create()
 
-print('\nThe following directories were created\n')
+print("\nThe following directories were created\n")
 for directory in directories:
     create_directories(directory)
-    print(f'\t{directory}')
-print('\n')
+    print(f"\t{directory}")
+print("\n")
